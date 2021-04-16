c6ea471c7279e23a35f9f99bafca5dabfe46d7a5
Magnuti <magnustidemann@gmail.com>
2021-04-16 16:37:02 +0200

    Work on MST directions

--- moea/src/main/java/moea/MST.java
+++ moea/src/main/java/moea/MST.java
@@ -1,13 +1,16 @@
 package moea;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
+import moea.App.PixelDirection;
+
 public class MST {
-    final List<Map<Integer, Double>> edgeValues;
+    final double[][] edgeValues;
+    final int[][] neighborArrays;
     final int V;
 
     /**
@@ -15,12 +18,13 @@ public class MST {
      * 
      * @param adjacencyMatrix which is
      */
-    public MST(List<Map<Integer, Double>> edgeValues) {
+    public MST(double[][] edgeValues, int[][] neighborArrays) {
         this.edgeValues = edgeValues;
-        this.V = edgeValues.size();
+        this.neighborArrays = neighborArrays;
+        this.V = edgeValues.length;
     }
 
-    void primMST(int startingNode) {
+    private int[] primMST(int startingNode) {
         int parents[] = new int[V];
         double keys[] = new double[V];
 
@@ -36,39 +40,63 @@ public class MST {
 
         while (!unvisitedNodes.isEmpty()) {
             // Extract min node from the graph of unvisited nodes
-            int u = -1;
+            // Note that we should this EXTRACT_MIN function more efficienly if we are to
+            // optimize the code. Maybe with a binary heap or a Fibonacci heap.
+            int nextNode = -1;
             double min = Double.POSITIVE_INFINITY;
-
             for (Integer i : unvisitedNodes) {
                 if (keys[i] < min) {
                     min = keys[i];
-                    u = i;
+                    nextNode = i;
                 }
             }
 
-            visitedNodes.add(u);
-            unvisitedNodes.remove(u);
+            visitedNodes.add(nextNode);
+            unvisitedNodes.remove(nextNode);
 
-            for (Integer neighborIndex : edgeValues.get(u).keySet()) {
-                if (unvisitedNodes.contains(neighborIndex)) {
-                    double edgeWeight = edgeValues.get(u).get(neighborIndex);
-                    if (edgeWeight < keys[neighborIndex]) {
-                        parents[neighborIndex] = u;
-                        keys[neighborIndex] = edgeWeight;
+            for (int n = 0; n < this.neighborArrays[nextNode].length; n++) {
+                int neighborNode = this.neighborArrays[nextNode][n];
+                if (unvisitedNodes.contains(neighborNode)) {
+                    double edgeWeight = edgeValues[nextNode][n];
+                    if (edgeWeight < keys[neighborNode]) {
+                        parents[neighborNode] = nextNode;
+                        keys[neighborNode] = edgeWeight;
                     }
                 }
             }
 
         }
 
-        System.out.println("Started from node " + startingNode);
-        System.out.println("From - to \tWeight");
-        for (int i = 0; i < 10; i++) {
-            int toIndex = visitedNodes.get(i);
-            int fromIndex = parents[toIndex];
-            if (fromIndex != -1) {
-                System.out.println(fromIndex + " - " + toIndex + "\t\t" + edgeValues.get(fromIndex).get(toIndex));
+        // System.out.println("Started from node " + startingNode);
+        // System.out.println("From - to \tWeight");
+        // for (int i = 0; i < 10; i++) {
+        // int toIndex = visitedNodes.get(i);
+        // int fromIndex = parents[toIndex];
+        // if (fromIndex != -1) {
+        // System.out.println(fromIndex + " - " + toIndex + "\t\t" +
+        // edgeValues.get(fromIndex).get(toIndex));
+        // }
+        // }
+        return parents;
+    }
+
+    PixelDirection[] findDirections(int startingNode) {
+        int[] parents = primMST(startingNode);
+
+        PixelDirection[] directions = new PixelDirection[this.V];
+        for (int i = 0; i < parents.length; i++) {
+            int parentNode = parents[i];
+
+            // Dumb find index method, fix later
+            int x = -1;
+            for (int k = 0; k < this.neighborArrays[i].length; k++) {
+                if (this.neighborArrays[i][k] == parentNode) {
+                    x = k;
+                }
             }
+            // int x = Arrays.asList(this.neighborArrays[i]).indexOf(parentNode);
+            directions[i] = PixelDirection.values()[x];
         }
+        return directions;
     }
 }
